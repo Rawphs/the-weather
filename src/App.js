88daa7b1d44000e3a5e8af129ac25bdb23cf6727
Rawphs <raphaelaprotasio@gmail.com>
2018-10-09 12:51:14 +0200

    feat(App): implement search by user input and geolocation and display cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,121 @@
+import {Grid, Typography} from '@material-ui/core';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import AutosuggestInput from './Components/AutosuggestInput';
+import WeatherCard from './Components/WeatherCard';
+import apiService from './Services/apiService';
+
+const styles = () => ({
+  root    : {
+    height  : 250,
+    flexGrow: 1,
+  },
+  forecast: {
+    marginTop: 20,
+  },
+});
 
 class App extends Component {
-  render() {
+  state = {
+    forecast : null,
+    city: '',
+    country: '',
+  };
+
+  componentDidMount () {
+    if (navigator.geolocation) {
+      console.log('location');
+      navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+        if (coords) {
+          const response = await apiService.fetchForecastByCoords({ coords });
+
+          if (response.cod === '200') {
+            this.setState({ ...this.parseResponse(response) });
+          }
+        }
+      }, console.log);
+    }
+    else {
+      console.log('Geolocation is not supported for this Browser/OS.');
+    }
+  }
+
+  parseResponse (response) {
+    let parsed = {
+      city: response.city.name,
+      country: response.city.country,
+    };
+
+    parsed.forecast = response.list.reduce((prev, current) => {
+      const currentDate = new Date(current.dt_txt);
+      const date        = currentDate.getDate();
+
+      if (!prev[date]) {
+        prev[date] = [];
+      }
+
+      prev[date].push({
+        icon: current.weather[0].icon,
+        ...current.main,
+        time: `${currentDate.getHours()}h`,
+        date: currentDate,
+      });
+
+      return prev;
+    }, {});
+
+    return parsed;
+  }
+
+  async onSelect (id) {
+    const response = await apiService.fetchForecastById(id);
+
+    if (response.cod === '200') {
+      this.setState({ ...this.parseResponse(response) });
+    }
+  }
+
+  renderTitle() {
+    if (!this.state.city) {
+      return;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Typography component="h2" variant="h2" align="center">
+        {this.state.city}, {this.state.country}
+      </Typography>
+    );
+  }
+
+  renderForecast () {
+    const { forecast } = this.state;
+
+    if (!forecast) {
+      return;
+    }
+
+    return (
+      <Grid container spacing={16} className={this.props.classes.forecast} justify="center">
+        {Object.keys(forecast).map(date => {
+          return (
+            <Grid item key={`card-${date}`}>
+              <WeatherCard {...forecast[date][0]} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
+  }
+
+  render () {
+    return (
+      <div className={this.props.classes.root}>
+        <AutosuggestInput onSelect={id => this.onSelect(id)} />
+        {this.renderTitle()}
+        {this.renderForecast()}
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
