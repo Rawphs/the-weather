0386031135fdc7d0fce15f9a2fcdf006bb72ef31
Rawphs <raphaelaprotasio@gmail.com>
2018-10-09 12:45:11 +0200

    feat(Images): remove react logo and add weather images

---
+++ src/Images/index.js
@@ -0,0 +1,11 @@
+export default {
+  '01': require('./039-sun.png'),
+  '02': require('./038-cloudy-3.png'),
+  '03': require('./001-cloud.png'),
+  '04': require('./011-cloudy.png'),
+  '09': require('./004-rainy-1.png'),
+  '10': require('./003-rainy.png'),
+  '11': require('./013-storm-2.png'),
+  '13': require('./006-snowy.png'),
+  '50': require('./017-foog.png'),
+}
